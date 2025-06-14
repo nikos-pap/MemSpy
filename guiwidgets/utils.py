65b5f04b5e267fc9efa15afd9cbac7c2468db2d3
nickp3065 <nickp3065@gmail.com>
2025-06-14 16:53:54 +0300

    added emoji icons

--- guiwidgets/utils.py
+++ guiwidgets/utils.py
@@ -1,3 +1,5 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QIcon, QPixmap, QPainter, QFont
 from numba.cuda import CudaSupportError
 from numba.cuda.cudadrv.driver import CudaAPIError
 from numba.cuda.cudadrv.error import CudaDriverError
@@ -70,3 +72,21 @@ class SavedTreeTypes(Enum):
     POINTER = auto()
     GROUP = auto()
     ADDRESS = auto()
+
+
+def emoji_icon(emoji: str, size: int = 32) -> QIcon:
+    """
+    Create a QIcon by painting the given emoji onto a transparent pixmap.
+    """
+    pixmap = QPixmap(size, size)
+    pixmap.fill(Qt.GlobalColor.transparent)
+
+    painter = QPainter(pixmap)
+    # pick a font large enough to fill the pixmap
+    font = QFont()
+    font.setPixelSize(int(size * 0.8))
+    painter.setFont(font)
+    painter.drawText(pixmap.rect(), Qt.AlignmentFlag.AlignCenter, emoji)
+    painter.end()
+
+    return QIcon(pixmap)
