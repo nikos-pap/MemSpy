de02686244e36e809190c5644aa6f5a87cfde940
nickp3065 <nickp3065@gmail.com>
2025-11-22 14:18:09 +0200

    created scan parameters to match pointer scan parameters

--- src/memspy/utils/types/__init__.py
+++ src/memspy/utils/types/__init__.py
@@ -2,4 +2,4 @@ from memspy.utils.types.data_type import Type
 from memspy.utils.types.scan_type import ScanType
 from memspy.utils.types.message import MessageType
 from memspy.utils.types.workspace_types import PointerItem, AddressItem, ProcessItem, WorkspaceDataType, WorkspaceItem, WorkspaceGroupItem, SearchItem
-from memspy.utils.types.pointer_scan_types import ModuleInfo, PointerScanParameters
+from memspy.utils.types.scan_types import ModuleInfo, PointerScanParameters
